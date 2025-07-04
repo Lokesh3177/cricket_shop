65c8d69cb4320206c5b0431c49c79028f9ec09e5
Lokesh3177 <lokesh31770@gmail.com>
2025-07-04 11:17:30 +0530

    crash-updated

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary'
-import productModel from "../models/productModel.js";
+import productModel from "../models/ProductModel.js";
 
 // function for Add product
 
