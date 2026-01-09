87cd6d856408dce962cc6a466e8fb8bc21c3230f
Lokesh3177 <lokesh31770@gmail.com>
2026-01-09 15:33:12 +0530

    Fix Navigate import

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './Components/Navbar'
 import SideBar from './Components/SideBar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Add from './Pages/Add'
 import List from './Pages/List'
 import Orders from './Pages/Orders'
 import Login from './Components/Login'
-import { ToastContainer, } from 'react-toastify';
-
+import { ToastContainer } from 'react-toastify'
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const currency = "$"
 
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
+  const [token, setToken] = useState(
+    localStorage.getItem('token') ? localStorage.getItem('token') : ''
+  )
+
   useEffect(() => {
     localStorage.setItem('token', token)
-
   }, [token])
 
   return (
@@ -26,24 +27,21 @@ const App = () => {
         token === ''
           ? <Login setToken={setToken} />
           : <>
-            <Navbar setToken={setToken} />
-            <hr />
-            <div className='w-full flex'>
-              <SideBar />
-              <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
-                <Routes>
-                  <Route path="/" element={<Navigate to="/list" />} />
-                  <Route path="/add" element={<Add token={token} />} />
-                  <Route path="/list" element={<List token={token} />} />
-                  <Route path="/orders" element={<Orders token={token} />} />
-                </Routes>
+              <Navbar setToken={setToken} />
+              <hr />
+              <div className='w-full flex'>
+                <SideBar />
+                <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+                  <Routes>
+                    <Route path="/" element={<Navigate to="/list" />} />
+                    <Route path="/add" element={<Add token={token} />} />
+                    <Route path="/list" element={<List token={token} />} />
+                    <Route path="/orders" element={<Orders token={token} />} />
+                  </Routes>
+                </div>
               </div>
-            </div>
-          </>
-
+            </>
       }
-
-
     </div>
   )
 }
