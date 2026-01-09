87433b4666ffad56eaadd5a02c5e922234b8c557
Lokesh3177 <lokesh31770@gmail.com>
2026-01-09 15:20:36 +0530

    Add default route for admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -32,9 +32,10 @@ const App = () => {
               <SideBar />
               <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
                 <Routes>
-                  <Route path='/add' element={<Add token={token} />} />
-                  <Route path='/list' element={<List token={token} />} />
-                  <Route path='/orders' element={<Orders token={token} />} />
+                  <Route path="/" element={<Navigate to="/list" />} />
+                  <Route path="/add" element={<Add token={token} />} />
+                  <Route path="/list" element={<List token={token} />} />
+                  <Route path="/orders" element={<Orders token={token} />} />
                 </Routes>
               </div>
             </div>
