77c0c0ff1f0bd419c96793b3a88bd6fc64bf8325
Lokesh3177 <lokesh31770@gmail.com>
2026-07-24 10:41:11 +0530

    cart updated

--- client/src/Pages/Cart.jsx
+++ client/src/Pages/Cart.jsx
@@ -29,7 +29,7 @@ const Cart = () => {
 
     }
 
-  }, [cartItems]);
+  }, [cartItems, Products]);
 
 
 
@@ -42,9 +42,9 @@ const Cart = () => {
       <div>
         {
           cartData.map((item, index,) => {
-            const productData = Products.find((product) => product._id === item._id
+            const productData = Products.find((product) => product._id === item._id);
+            if (!productData) return null;
 
-            );
             return (
               <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
                 <div className='flex items-start gap-6'>
